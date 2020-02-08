cb312b505bba814a2ff95938b4a785c49552f517
Mikhail Zhirnov <antiphysic@gmail.com>
2020-02-09 02:14:16 +0300

    Create retrofit interface

---
+++ app/src/main/java/com/example/russiannewsapp/retrofit/NewsService.kt
@@ -0,0 +1,11 @@
+package com.example.russiannewsapp.retrofit
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface NewsService {
+
+@get : GET("v2/sources?country=ru&apiKey=e6d7188a10a34f8aaf4f130346a337d5")
+val sources: Call<NewsWebsite>
+
+}
---
+++ app/src/main/java/com/example/russiannewsapp/retrofit/RetrofitClient.kt
@@ -0,0 +1,19 @@
+package com.example.russiannewsapp.retrofit
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Url
+
+object RetrofitClient {
+
+    private var retrofit: Retrofit? = null
+
+    fun getClient(baseUrl: String?) : Retrofit {
+ if (retrofit == null) {
+     retrofit = Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(GsonConverterFactory.create())
+         .build()
+ }
+
+return  retrofit!!
+    }
+}
